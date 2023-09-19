1881d4e909dd7136061fe3543c48a90697b808e0
antoni-devlin <antoni.devlin@gmail.com>
2023-09-19 12:54:40 +0100

    wip - having trouble fetching and using onMount

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,37 @@
 <script>
+import { onMount } from 'svelte';
+
+let userId = "105494066543086834878"; //My Google Books userId - this is publically available
+let fields =
+  "fields=items(id,volumeInfo(title,authors,publishedDate,description,pageCount,imageLinks(thumbnail),infoLink)";
+
+async function getShelves() {
+  let queryString = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves`;
+  let response = await fetch(queryString);
+  return response.json();
+}
+
+async function getBooks(bookshelfId) {
+  let queryString = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${bookshelfId}/volumes?${fields})`;
+  let response = await fetch(queryString);
+  return response.json();
+}
+
+function constructBooks(jsonResponse) {
+  let ids = jsonResponse["items"].map((x) => x.id);
+  let titles = jsonResponse["items"].map((x) => x.volumeInfo.title);
+  console.log(ids, titles);
+}
+
+onMount(constructBooks(getBooks(4)))
 
 </script>
 
 <h1>About</h1>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nam aliquid incidunt dolor eveniet error et magnam porro alias numquam omnis animi, eos quia, voluptates velit rem ratione perspiciatis nulla!</p>
+
+<h2> Book list </h2>
 
 
 

104475878ba5508d460521b5735d34250779f023
antoni-devlin <antoni.devlin@gmail.com>
2023-09-19 12:49:26 +0100

    Added array of projects to Projects page, and a Svelte loop to create a project list

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,22 @@
 <script>
 
+  let projects = [
+  {name: "Project 1", slug: null, language: "Node", id: 0},
+  {name: "Project 2", slug: null, language: "Python", id: 1},
+  {name: "Project 3", slug: null, language: "Rubyh", id: 2}
+];
+
 
 </script>
 
 <h1>Projects</h1>
 
 <ul>
+  {#each projects as project (project.id)}
+  <li><a href="/projects/{project.slug}">{project.name} ({project.language})</a></li>
+  {:else}
+  No projects!
+  {/each}
 </ul>
 
 <style>

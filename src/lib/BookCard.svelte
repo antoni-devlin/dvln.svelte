eb0b24d033c9783f4d404061c937c1069cd74ee5
Antoni <antoni-devlin@users.noreply.github.com>
2023-11-21 13:20:18 +0000

    Created BookCard component

---
+++ src/lib/BookCard.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let title, author, thumb, desc, infoLink;
+</script>
+
+<div class="book">
+  <h2>{title}</h2>
+  <h3>{author}</h3>
+  <img src={thumb} alt="" />
+  <p>{desc} <a href={infoLink}>Read more</a>.</p>
+</div>
+
+<style>
+  h2 {
+    padding: 0;
+    margin: 0;
+  }
+
+  h3 {
+    font-size: 90%;
+    padding: 0;
+    margin: 0;
+  }
+</style>

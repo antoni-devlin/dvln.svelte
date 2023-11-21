8f612600df79278e3a0d594c506196b1c7efbf8e
Antoni <antoni-devlin@users.noreply.github.com>
2023-11-21 13:18:44 +0000

    Spacing fixed

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,50 +1,49 @@
 <script>
   let menuItems = [
-    {linkText: "Home", url:"/", id: "0"},
-    {linkText: "About", url:null, id: "1"},
-    {linkText: "Past work", url:null, id: "2"},
-    {linkText: "Projects", url:null, id: "3"}
-  ]
+    { linkText: "Home", url: "/", id: "0" },
+    { linkText: "About", url: null, id: "1" },
+    { linkText: "Past work", url: null, id: "2" },
+    { linkText: "Projects", url: null, id: "3" },
+  ];
   for (const item of menuItems) {
     if (item.linkText !== "Home") {
-    item["url"] = `/${item.linkText.replaceAll(" ", "-").toLowerCase()}`;
+      item["url"] = `/${item.linkText.replaceAll(" ", "-").toLowerCase()}`;
+    }
   }
-}
 </script>
 
 <nav>
   <menu>
-    {#each menuItems as link (menuItems.id)}
-    <li><a href="{link.url}">{link.linkText}</a></li> |
+    {#each menuItems as link (link.id)}
+      <li><a href={link.url}>{link.linkText}</a></li>
+      |
     {:else}
-    No links!
+      No links!
     {/each}
   </menu>
 </nav>
 
 <style>
+  li a {
+    text-decoration: none;
+  }
 
-li a {
-  text-decoration: none;
-}
+  menu {
+    list-style: none;
+  }
 
-menu {
-  list-style: none;
-}
+  li {
+    display: inline-block;
+    /* padding: 15px; */
+    color: black;
+  }
 
-li {
-  display: inline-block;
-  /* padding: 15px; */
-  color: black;
-}
+  a {
+    padding: 15px;
+  }
 
-a {
-  padding: 15px;
-}
-
-a:hover {
-  color: white;
-  background: black;
-}
-
+  a:hover {
+    color: white;
+    background: black;
+  }
 </style>

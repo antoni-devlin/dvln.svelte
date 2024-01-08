19531c0d5724521da33832fd5a055c263dce2a96
antoni-devlin <antoni.devlin@gmail.com>
2024-01-08 18:08:57 +0000

    Had to undo SSG code as it was giving me loads of vesion conflict errors

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,27 @@
 // Default "auto" adapter
 // import adapter from "@sveltejs/adapter-auto";
 
-// Static adapter
-import adapter from "@sveltejs/adapter-static";
+// // Static adapter
+// import adapter from "@sveltejs/adapter-static";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
-  },
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter({
+//       pages: "build",
+//       assets: "build",
+//       fallback: undefined,
+//       precompress: false,
+//       strict: true,
+//     }),
+//   },
+// };
+
+// Default config
+
+import adapter from "@sveltejs/adapter-auto";
+/** @type {import('@sveltejs/kit').Config} */ const config = {
+  kit: { adapter: adapter() },
 };
 
 export default config;

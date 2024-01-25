2a0ab8e0d966355e74c6168d35f477b3a8d5b143
Antoni <antoni-devlin@users.noreply.github.com>
2024-01-25 14:13:24 +0000

    wip

--- src/routes/past-work/+page.svelte
+++ src/routes/past-work/+page.svelte
@@ -1,11 +1,7 @@
 <script>
-
-
 </script>
 
 <h1>Past work</h1>
 
 <style>
-
-
 </style>
---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let data;
+  const { posts } = data;
+</script>
+
+{#await posts}
+  Loading
+{:then posts}
+  {posts}
+{/await}

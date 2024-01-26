c7cc55223c28ddcffcf60b1781f914033f92e429
Antoni <antoni-devlin@users.noreply.github.com>
2024-01-26 15:27:10 +0000

    Made progress!

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,12 +1,11 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load({ params }) {
-  console.log(params);
-
   const { data, error } = await supabase
     .from("posts")
-    .select("slug")
-    .eq(params.slug);
+    .select("*")
+    .eq("slug", params.slug);
+  console.log(data);
   return {
     post: data,
   };
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   export let data;
   const { post } = data;
-  console.log(data);
 </script>
 
-<h1>{post}</h1>
+{post.title}

0e87d9b1a3ce5e16e2a660a2c96fbe950705540d
antoni-devlin <antoni.devlin@gmail.com>
2024-01-27 15:34:57 +0000

    wip

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,5 +1,11 @@
 <script>
   export let data;
+  let post = data;
+
+  import dayjs from "dayjs";
+  post.created_at = dayjs(post.created_at).format("DD/MM/YYYY");
 </script>
 
-{data.title}
+<h1>{post.title}</h1>
+<span>Posted: {post.created_at}</span>
+<p>{@html post.body}</p>

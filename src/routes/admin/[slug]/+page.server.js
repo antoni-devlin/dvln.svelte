bcc8fe8980313c0604e0aa514f7003d7b590652b
antoni-devlin <antoni.devlin@gmail.com>
2024-02-14 13:03:13 +0000

    MVP post updating works!

--- src/routes/admin/[slug]/+page.server.js
+++ src/routes/admin/[slug]/+page.server.js
@@ -7,25 +7,20 @@ export const load = ({ params }) => {
   return data;
 };
 
-import { fail } from '@sveltejs/kit'
+import { fail } from "@sveltejs/kit";
 
 export const actions = {
-	default: async ({request, params}) => {
-		const formData = await request.formData();
+  default: async ({ request, params }) => {
+    const formData = await request.formData();
 
-    const title = formData.get('title')
-    const body = formData.get('body')
-    console.log(params.slug)
-    const { data, error } = await supabase
-    .from("posts")
-    .update({ title: title, body: body })
-    .eq("slug", params.slug)
-    return data
-	}
+    const title = formData.get("title");
+    const body = formData.get("body");
+    const { error } = await supabase
+      .from("posts")
+      .update({
+        title: title,
+        body: body,
+      })
+      .eq("slug", params.slug);
+  },
 };
-
-// export const actions = {
-//   default: async (event) => {
-//     // Update post you're currently editing
-//   },
-// };
--- src/routes/admin/[slug]/+page.svelte
+++ src/routes/admin/[slug]/+page.svelte
@@ -1,26 +1,29 @@
 <script>
-  import Tiptap from "$lib/Tiptap.svelte";
-  import { enhance } from '$app/forms'
+  // import Tiptap from "$lib/Tiptap.svelte";
+  import { enhance } from "$app/forms";
   export let data;
   let post = data;
-  let slug = post.slug;
+  // let slug = post.slug;
   import dayjs from "dayjs";
   post.created_at = dayjs(post.created_at).format("DD/MM/YYYY");
 </script>
 
-<form method="POST" use:enhance>
-	<label>
-		Title
-		<input name="title" type="text">
-	</label>
-	<label>
-		Body
-		<textarea name="body" rows="4" cols="50"/>
-	</label>
-	<button>Log in</button>
-</form>
+<h2>Edit post</h2>
 
-<!-- <label>
+<form method="POST" use:enhance>
+  <label>
+    Title
+    <br />
+    <input name="title" type="text" value={post.title} />
+  </label>
+  <br />
+  <br />
+  <label>
     Body
-    <Tiptap editorContent={post.body} />
-  </label> -->
+    <br />
+    <textarea name="body" value={post.body} rows="40" cols="100" />
+  </label>
+  <button>Log in</button>
+</form>
+<!-- 
+<Tiptap editorContent={post.body} /> -->

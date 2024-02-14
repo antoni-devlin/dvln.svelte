b79e2ed126c29c13c6816d7b6815e200f1c86769
antoni-devlin <antoni.devlin@gmail.com>
2024-02-14 12:56:54 +0000

    MVP new post creation form and posting to database works!

---
+++ src/routes/admin/new/+page.server.js
@@ -0,0 +1,32 @@
+import { supabase } from "$lib/supabaseClient";
+export const prerender = false;
+
+// export async function load() {
+//   const { data } = await supabase.from("posts").select();
+//   return {
+//     posts: data ?? [],
+//   };
+// }
+
+import { fail } from "@sveltejs/kit";
+
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+
+    const title = formData.get("title");
+    // const slug = title.replaceAll(" ", "-").toLowerCase();
+    const slug = title
+      .replace(/[^\w\s\']|_/g, "")
+      .replace(/\s+/g, " ")
+      .replaceAll(" ", "-")
+      .toLowerCase();
+    const body = formData.get("body");
+
+    const { error } = await supabase.from("posts").insert({
+      title: title,
+      body: body,
+      slug: slug,
+    });
+  },
+};
---
+++ src/routes/admin/new/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Tiptap from "$lib/Tiptap.svelte";
+  import { enhance } from "$app/forms";
+</script>
+
+<h2>Create new post</h2>
+
+<form method="POST" use:enhance>
+  <label>
+    Title
+    <input name="title" type="text" />
+  </label>
+  <label>
+    Body
+    <textarea name="body" rows="4" cols="50" />
+  </label>
+  <button>Save post</button>
+</form>

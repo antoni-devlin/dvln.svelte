575ea5f94ba61da75256e55c27f71c80eb011025
Antoni <antoni.devlin@gmail.com>
2024-02-23 16:10:40 +0000

    Set document.title for posts

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -6,6 +6,10 @@
   post.created_at = dayjs(post.created_at).format("DD/MM/YYYY");
 </script>
 
+<svelte:head>
+    <title>{post.title}</title> 
+</svelte:head>
+
 <h1>{post.title}</h1>
 <span>Posted: {post.created_at}</span>
 <p>{@html post.body}</p>

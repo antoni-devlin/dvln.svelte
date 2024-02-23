180909c1eebf12ee56873d623ffc636679ae88e2
Antoni <antoni.devlin@gmail.com>
2024-02-23 10:47:44 +0000

    Protected /admin/* routes with auth so only logged in users (i.e. me) can see it and edit posts

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,8 +1,16 @@
 import { supabase } from "$lib/supabaseClient";
+import { error, redirect } from "@sveltejs/kit";
 
-export async function load() {
+export const prerender = false;
+
+export async function load({locals: {getSession}}) {
+  const loggedin = await getSession()
+  if (!loggedin) {
+    throw error(401, { message: 'Unauthorized' })
+    // redirect(303, "/")
+  } else {
   const { data } = await supabase.from("posts").select();
   return {
     posts: data ?? [],
-  };
+  };}
 }

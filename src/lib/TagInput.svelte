1ea4a2b3163fc5c7319085574193b0b87ca6ed93
Antoni <antoni-devlin@users.noreply.github.com>
2024-02-27 09:03:03 +0000

    wip

---
+++ src/lib/TagInput.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div className="tags-input-container">
+  <div className="tag-item">
+    <span className="text">hello</span>
+    <span className="close">&times;</span>
+  </div>
+  <input type="text" className="tags-input" placeholder="Type somthing" />
+</div>
+
+<style>
+</style>

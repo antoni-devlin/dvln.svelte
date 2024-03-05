e18b79b5f30f3fea8a991d4b6eb4b3b34fdd7adb
Antoni <antoni-devlin@users.noreply.github.com>
2024-03-05 15:24:02 +0000

    Styled login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,12 +3,31 @@
   export let form;
 </script>
 
-<h2>Log in</h2>
-<form action="?/login" method="POST" use:enhance>
-  <label for="email">email</label>
-  <input name="email" type="email" value={form?.email ?? ""} required />
-  <label for="password">password</label>
-  <input name="password" required />
-  <button type="submit">Login</button>
-</form>
+<div class="container-fluid w-25">
+  <h2>Log in</h2>
+  <form action="?/login" method="POST" use:enhance>
+    <div class="form-floating mb-3">
+      <input
+        type="email"
+        class="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+        value={form?.email ?? ""}
+        required
+      />
+      <label for="floatingInput">Email address</label>
+    </div>
+
+    <div class="form-floating mb-3">
+      <input
+        type="password"
+        class="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+      />
+      <label for="floatingPassword">Password</label>
+      <button class="btn btn-primary mt-3" type="submit">Login</button>
+    </div>
+  </form>
+</div>
 {#if form?.invalid}<mark>{form?.message}!</mark>{/if}

48a6db267fcb9546eebee3fad51521f1de27006d
Antoni <antoni-devlin@users.noreply.github.com>
2024-03-08 14:56:20 +0000

    Updated all supabase querys to get a list of posts so only those with a publishing_status other than "deleted" are shown.

--- src/routes/posts/+page.server.js
+++ src/routes/posts/+page.server.js
@@ -1,7 +1,10 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("posts").select().eq('publishing_status', 'published');
+  const { data } = await supabase
+    .from("posts")
+    .select()
+    .eq("publishing_status", "published");
   return {
     posts: data ?? [],
   };

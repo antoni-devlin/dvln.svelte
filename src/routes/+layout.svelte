138444bc92994dbbef74930335304e6f62e5a36c
Antoni <antoni-devlin@users.noreply.github.com>
2024-04-18 10:12:53 +0000

    Added global main.css and linked it in root layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,6 +64,7 @@
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
   />
+  <link rel="stylesheet" href="/styles/main.css" />
 </svelte:head>
 
 {#if PUBLIC_ENV === "DEV"}
@@ -104,27 +105,16 @@
     font-weight: 900;
   }
 
-  li a {
-    text-decoration: none;
-    color: black;
-  }
-
-  menu {
-    list-style: none;
-  }
-
   li {
     display: inline-block;
-    /* padding: 15px; */
-    color: black;
+  }
+
+  li a {
+    text-decoration: none;
+    color: black !important;
   }
 
   a {
     padding: 15px;
-  }
-
-  a:hover {
-    color: white;
-    background: black;
   }
 </style>

a601686463e3ddb801a49bf12bf347ce868b0200
Antoni <antoni-devlin@users.noreply.github.com>
2024-04-22 13:01:45 +0000

    Added markdown rendering on the frontend using Markdoc

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,13 +1,24 @@
 <script>
   export let data;
   let post = data;
-
+  import Markdoc from "@markdoc/markdoc";
   import dayjs from "dayjs";
   post.created_at = dayjs(post.created_at).format("DD/MM/YYYY");
+  function rendermd(source) {
+    const config = {};
+    const ast = Markdoc.parse(source);
+    const content = Markdoc.transform(ast, config);
+
+    const html = Markdoc.renderers.html(content);
+    return html;
+  }
 </script>
 
 <div class="container w-90 mt-5">
   <h2 class="display-2">{post.title}</h2>
   <small class="text-body-secondary">{post.created_at}</small>
-  <p class="pt-1">{@html post.body}</p>
+  <p class="pt-1">{@html rendermd(post.body)}</p>
 </div>
+
+<style>
+</style>

a16021bf66e4ca1ee0b8b81dc36bc55d1c2a2e6c
Antoni <antoni-devlin@users.noreply.github.com>
2024-04-22 14:24:38 +0000

    Removed some comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,6 @@
 
   onMount(async () => {
     supabase.auth.onAuthStateChange((event, _session) => {
-      // invalidate("supabase:auth");
       invalidateAll();
     });
     return () => subscription.unsubscribe();
@@ -24,32 +23,10 @@
     cancel();
     await goto("/");
   };
-  // leaving this here to discuss with Hugh
-  // let menuItems = [
-  //   { linkText: "Home", url: "/", id: "0" },
-  //   { linkText: "Projects", id: "1" },
-  //   { linkText: "Posts", id: "2" },
-  // ];
-
-  // if (data.session) {
-  //   menuItems.push({ linkText: "Admin", id: "3" });
-  // } else {
-  //   menuItems.push({ linkText: "Login", id: "4" });
-  // }
-
-  // for (const item of menuItems) {
-  //   if (item.linkText !== "Home") {
-  //     item["url"] = `/${item.linkText.replaceAll(" ", "-").toLowerCase()}`;
-  //   }
-  // }
 
   $: menuItems = [
     { linkText: "Home", url: "/", id: "0" },
-    // { linkText: "Projects", id: "1" },
     { linkText: "Posts", id: "2" },
-    // ...(session
-    //   ? [{ linkText: "Admin", id: "3" }]
-    //   : [{ linkText: "Login", id: "4" }]),
   ].map((item) => ({
     ...item,
     url:

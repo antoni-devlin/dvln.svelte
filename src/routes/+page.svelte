5d656c03cd341673c458003d2db7c6e3d88c62cf
Antoni <antoni-devlin@users.noreply.github.com>
2024-04-23 12:59:06 +0000

    Updated static content on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,39 @@
 <script>
   let name = "Antoni";
-  let tagline =
-    "I write stuff and also build stuff. I like trying to do the most with the least.";
 </script>
 
-<h1>Hi, my name's {name}</h1>
-<h2>{tagline}</h2>
-<p>
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis corrupti
-  magni delectus odit aut! Quo facilis repellendus soluta dolorem mollitia ea
-  iusto laborum, unde illum non totam nesciunt eius est.
-</p>
+<div class="container-fluid home-content w-60 text-center">
+  <img
+    class="w-10"
+    src="https://avatars.githubusercontent.com/u/11968957?v=4"
+    alt=""
+  />
+  <h1 class="text-start">
+    Hi, my name's Antoni <span class="pronouns">(he/him)</span>
+  </h1>
+  <h2 class="text-start">
+    I write stuff, build stuff and like trying to do the most with the least.
+  </h2>
+  <p class="text-start">
+    I'm currently a Content Designer in the <a
+      href="https://www.gov.uk/government/organisations/government-digital-service/about"
+      >Government Digital Service (GDS)</a
+    >, working on <a href="https://gov.uk">GOV.UK</a>.
+  </p>
+</div>
 
 <style>
+  .home-content > img {
+    clip-path: circle(50% at 50% 50%);
+  }
+  .pronouns {
+    color: rgba(0, 0, 0, 0.5);
+    font-size: 20px;
+  }
+
+  h1,
+  h2,
+  img {
+    margin-bottom: 20px;
+  }
 </style>

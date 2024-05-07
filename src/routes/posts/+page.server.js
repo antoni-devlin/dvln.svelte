e83fea3dc1d13154240e2c153f9c3578d3b77a7e
Antoni <antoni-devlin@users.noreply.github.com>
2024-05-07 14:07:31 +0000

    Modified load function on /posts It now: + grabs the "search" query parameter from the url, and uses it to perform a psql full-text search in supabase

--- src/routes/posts/+page.server.js
+++ src/routes/posts/+page.server.js
@@ -4,13 +4,20 @@ let table;
 
 PUBLIC_ENV === "DEV" ? (table = "dev_posts") : (table = "posts");
 
-export async function load() {
-  const { data } = await supabase
+export async function load({ url }) {
+  let q = url.searchParams.get("search");
+  const supabaseQuery = supabase
     .from(table)
     .select()
     .eq("publishing_status", "published")
     .not("body", "is", null)
     .order("created_at", { ascending: false });
+
+  if (q && !(q == " ")) {
+    // supabaseQuery.ilike("title", `%${q}%`);
+    supabaseQuery.textSearch("fts", `${q}:*`, { type: "websearch" });
+  }
+  const { data } = await supabaseQuery;
   return {
     posts: data ?? [],
   };

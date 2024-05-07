cdfb7f418a5cb74b7e284a3fc3dc356805dd3cdf
Antoni <antoni-devlin@users.noreply.github.com>
2024-05-07 14:08:56 +0000

    Display a message on /posts of no posts are loaded

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -8,14 +8,18 @@
 </svelte:head>
 
 <div class="container mt-5">
-  {#each posts as post}
-    <div class="post w-80 p-10">
-      <h2>{post.title}</h2>
-      <p>{post.excerpt}</p>
-      <a href="/posts/{post.slug}">Read more</a>
-      <hr />
-    </div>
-  {/each}
+  {#if posts.length > 0}
+    {#each posts as post}
+      <div class="post w-80 p-10">
+        <h2>{post.title}</h2>
+        <p>{post.excerpt}</p>
+        <a href="/posts/{post.slug}">Read more</a>
+        <hr />
+      </div>
+    {/each}
+  {:else}
+    <div class="post w-80 p-10"><p>Could not find any posts</p></div>
+  {/if}
 </div>
 
 <style>

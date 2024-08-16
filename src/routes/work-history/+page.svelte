789a5e9a920eebb1a680e7db2306f46019d6f558
Antoni <antoni-devlin@users.noreply.github.com>
2024-08-16 13:49:10 +0000

    Created work history page

---
+++ src/routes/work-history/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+</script>
+
+<h1>Work history</h1>
+
+<p>
+  I am a solutions-focused writer and content designer with an eye for detail
+  and the proven ability to get to the root of content design problems by
+  “following the confusion”. I have a knack for finding annoyances and
+  inefficiencies in processes, and coming up with bare-bones and budget-friendly
+  improvements. I’m also a self-taught programmer, and have used this knowledge
+  to improve processes for myself and my colleagues.
+</p>
+
+<h2>Experience</h2>
+
+<div class="job">
+  <h2 class="job-title">
+    Content Designer at <a
+      href="https://www.gov.uk/government/organisations/government-digital-service"
+      >Government Digital Service (GDS)</a
+    >
+  </h2>
+  <p class="time-period">March 2022 - Present</p>
+  <p class="job-summary">
+    I use evidence, data, and user research to inform my content design
+    decisions, and ensure user needs are the primary driver of the content I
+    write for GOV.UK. I also cultivate strong relationships with other
+    government departments to share knowledge and content design best practice.
+    I actively contribute to and adhere to the GOV.UK style guides and content
+    patterns. I also contribute to rota and other work where needed, such as
+    managing the content request queue, and working on government reshuffles out
+    of hours.
+  </p>
+</div>
+
+<div class="job">
+  <h2 class="job-title">
+    Digital Editor at <a href="https://gold.ac.uk"
+      >Goldsmiths, University of London</a
+    >
+  </h2>
+  <p class="time-period">November 2018 - March 2022</p>
+  <p class="job-summary">
+    I was responsible for managing and producing content for the Goldsmiths
+    website. This involved research, information synthesis, and structuring
+    content, as well as measuring impact once content was live. I worked closely
+    with academics to research and write engaging content about niche topics,
+    and presented it in a way that was true to the degree programme but
+    accessible to our target audiences. I also advised colleagues across the
+    institution on accessibility best practices when producing content for the
+    web.
+  </p>
+</div>
+
+<style>
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  .job-title {
+    font-size: 20px;
+    margin-bottom: 0;
+  }
+
+  .time-period {
+    color: rgb(105, 105, 105);
+  }
+
+  .job {
+    padding-bottom: 30px;
+  }
+</style>

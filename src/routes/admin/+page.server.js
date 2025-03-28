384e45a7191e5c5cf7f39c004e59fd52e557fbcc
Antoni <antoni-devlin@users.noreply.github.com>
2025-03-28 13:55:35 +0000

    Fixed login bypass bug on /admin routes. Moved /admin route protection to hooks.server.js for easier reuse.

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -7,19 +7,13 @@ PUBLIC_ENV === "DEV" ? (table = "dev_posts") : (table = "posts");
 
 export const prerender = false;
 
-export async function load({ locals: { getSession } }) {
-  const loggedin = await getSession();
-  if (!loggedin) {
-    throw error(401, { message: "Unauthorized" });
-    // redirect(303, "/")
-  } else {
-    const { data } = await supabase
-      .from(table)
-      .select()
-      .neq("publishing_status", "deleted")
-      .order("created_at", { ascending: false });
-    return {
-      posts: data ?? [],
-    };
-  }
+export async function load() {
+  const { data } = await supabase
+    .from(table)
+    .select()
+    .neq("publishing_status", "deleted")
+    .order("created_at", { ascending: false });
+  return {
+    posts: data ?? [],
+  };
 }

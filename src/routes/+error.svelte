e4edbb5a356297bc64ac24cf95df56110e520651
Antoni <antoni-devlin@users.noreply.github.com>
2025-03-28 13:54:33 +0000

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,18 @@
 <script>
-
+  export let error;
 </script>
 
-<h1>Page not found</h1>
-
-<style>
-  
-</style>
+{#if error?.status === 401}
+  <h1>Unauthorized</h1>
+  <p>
+    You do not have permission to access this page. Please <a href="/login"
+      >log in</a
+    > to continue.
+  </p>
+{:else if error?.status === 404}
+  <h1>Page Not Found</h1>
+  <p>The page you are looking for does not exist.</p>
+{:else}
+  <h1>An Error Occurred</h1>
+  <p>{error?.message ?? "Something went wrong."}</p>
+{/if}
